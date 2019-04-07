a5d5f2fd4613f76d7452214989f2c8ae25007bdc
Rafael <rafaelrdias@yahoo.com.br>
2019-04-07 00:11:02 -0300

    Atualizações Pontuais

--- 02_scripts/R/Analise_Qualidade_dos_Dados_E_Variaveis_Relevantes_R_V1.R
+++ 02_scripts/R/Analise_Qualidade_dos_Dados_E_Variaveis_Relevantes_R_V1.R
@@ -30,27 +30,37 @@ library(stringr)
 library(forecast)
 library(forecastHybrid)
 
+
+
 # Importando o arquivo proviniente da ETL para Análise da qualidade do dado
 
 export_graos <- data.table(read.csv2("https://raw.githubusercontent.com/datasciencealdeia/201903/master/03_dados/auxiliares/Base_AgroXP_0504.csv", header = T, sep=";"))
 
 View(export_graos)
 
+
+
 # Importando o arquivo com os Paises Importadores
 
 paises_import <- data.table(read.table("https://raw.githubusercontent.com/datasciencealdeia/201903/master/03_dados/auxiliares/Paises_Importadores.txt", header = T, sep=";", encoding = "UTF-8"))
 
+View(paises_import)
+
 # Criando Informações das NCM de soja, milho e café
 
-ncm <- data.table(CO_NCM=c(12019000,09011110,10059010), Nome=c("Soja","Cafe","Milho"))
+ncm <- data.table(CO_NCM=c(12019000,9011110,10059010), Nome=c("Soja","Cafe","Milho"))
 
-ncm
+View(ncm)
+
+
 
 # Analisando a Qualidade dos Dados
 
 ## Nomes dos Campos
 
 names(export_graos)
+
+
 
 # Valores Máximos e Mínimos dos principais campos
 
@@ -60,15 +70,22 @@ min(export_graos$CO_ANO)
 
 max(export_graos$CO_ANO)
 
+
+
 ## Toneladas
 
 min(export_graos$KG_LIQUIDO)
 
 max(export_graos$KG_LIQUIDO)
 
+
+
   # Gráfico para entender melhor a distribuição, dado o alto valor de 1798445645 Toneladas
 
   plot(export_graos$KG_LIQUIDO)
+  
+  
+  
 
 ## Valor em Dólares
 
@@ -76,15 +93,25 @@ min(export_graos$VL_FOB)
 
 max(export_graos$VL_FOB)
 
+
+
+
 ## Cotação do Dólar
 
 min(export_graos$Fechamento)
 
 max(export_graos$Fechamento)
 
+
+
+
+
 ## Ou prode-se fazer isto no atacado
 
 summary(export_graos)
+
+
+
 
 ## Como não existem problemas aparentes nos dados, eles serão explorados
 
@@ -98,9 +125,13 @@ import <- data.frame((export_graos %>%
 
 import
 
+
+
 ## Agrupamento gerou vários valores nulos, serão substituídos
 
 import$Dolares[which(is.na(import$Dolares))] <- 0
+
+
 
 ## Armazenando e encontrando o Pais
 
@@ -108,11 +139,16 @@ m_v <- max(import)
 
 import[import$Dolares == m_v,]
 
+
+
 ## Procurando o pais no arquivo/tabela referência
 
 m_p_valor <- paises_import[paises_import$X.U.FEFF.CO_PAIS==764,]
 
 m_p_valor
+
+
+
 
 ## E o maior importador em Toneladas?
 
@@ -126,15 +162,18 @@ import_2 <- data.frame((export_graos %>%
 
 import_2
 
+
 ## Agrupamento gerou vários valores nulos, serão substituídos
 
 import_2$Toneladas[which(is.na(import_2$Toneladas))] <- 0
 
-## Armazenando e encontrando o Pais
+
+## Encontrando e armazenando o Pais
 
 m_v <- max(import_2)
 
 import_2[import_2$Toneladas == m_v,]
+
 
 ## Procurando o pais no arquivo/tabela referência
 
@@ -177,7 +216,9 @@ m_i_cafe
 m_i_milho
 
 
-m_i_ton_scm <- paises_import[paises_import$X.U.FEFF.CO_PAIS==160 | paises_import$X.U.FEFF.CO_PAIS==23 | paises_import$X.U.FEFF.CO_PAIS==372,]
+m_i_ton_scm <- paises_import[paises_import$X.U.FEFF.CO_PAIS==160 | 
+                               paises_import$X.U.FEFF.CO_PAIS==23 | 
+                               paises_import$X.U.FEFF.CO_PAIS==372,]
 
 m_i_ton_scm
 
@@ -222,9 +263,12 @@ m_e_milho
 
 cor.test(export_graos$QT_ESTAT,export_graos$Fechamento)
 
+
 ## Verifica-se uma correlação direta entre estas variáveis
 
-corrplot.mixed(cor(data.frame(Quantidade=export_graos$QT_ESTAT,Dolar=export_graos$Fechamento)), number.cex = 1.5, upper = 'ellipse')
+corrplot.mixed(cor(data.frame(Quantidade=export_graos$QT_ESTAT,Dolar=export_graos$Fechamento)), 
+               number.cex = 1.5, upper = 'ellipse')
+
 
 ## Como a variável dolar explica a quantidade exportada?
 
@@ -254,6 +298,8 @@ print(qt_vs_dolar)
 summary(qt_vs_dolar)
 
 
+
+
 ## Como a variável CO_PAIS é nominal, não é possivel fazer esta análise
 
 ## Quantidade exportada e valor do grão no momento da venda
@@ -272,6 +318,9 @@ print(qt_vs_dolar)
 summary(qt_vs_dolar)
 
 
+
+
+
 # E se verificarmos Valor e dolar juntos com quantidade?
 
 
@@ -285,16 +334,24 @@ print(qt_vs_valor_e_dolar)
 summary(qt_vs_valor_e_dolar)
 
 
+
+
+
 # Conseguimos cruzar todas as variáveis e validar todas de uma única vez
 ## Basta separar todas as variáveis numéricas numa única tabela
 
 
-tab_cor <- data.frame(Quantidade=export_graos$QT_ESTAT,Valor=export_graos$VL_FOB, Dolar=export_graos$Fechamento, Toneladas=export_graos$KG_LIQUIDO)
+tab_cor <- data.frame(Quantidade=export_graos$QT_ESTAT,
+                      Valor=export_graos$VL_FOB, 
+                      Dolar=export_graos$Fechamento, 
+                      Toneladas=export_graos$KG_LIQUIDO)
 
 
 ## E fazer a análise de correlação
 
 corrplot.mixed(cor(tab_cor), number.cex = 1.5, upper = 'ellipse')
+
+
 
 
 ## Aqui verifica-se que existe relação direta entre toneladas comercializadas e o valor do dolar
@@ -306,6 +363,9 @@ print(ton_vs_dolar)
 summary(ton_vs_dolar)
 
 
+
+
+
 ## Neste caso uma variável não é suficiente para explicar a outra
 
-# Conclusão: Existem outras variáveis exógenas que necessitam ser encontradas para melhorar o ajuste
+# Conclusão: Existem outras variáveis que necessitam ser encontradas e anexadas para explicar as vendas de grãos
